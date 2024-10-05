37e7aa5652766b8705e8804c805f5598cd5c7412
= <elkhalfaoui@gmail.com>
2024-10-05 16:50:07 +0100

    starts again

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,4 @@
-import './App.css';
-import Header from './header/Header';
-import Hero from './hero/Hero';
-import profile from './assets/zakaria.jpg';
-import About from './about/About';
-import Skills from './skills/Skills';
-
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <About />
-      <Skills />
-    </>
-  );
+  return <div>App</div>;
 };
 export default App;
