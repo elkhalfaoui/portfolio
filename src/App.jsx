206dccc437878008eb8e29f4776942fdf5988583
= <elkhalfaouizakaria@gmail.com>
2024-11-18 22:07:42 +0100

    portfolio 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import About from "./about/About";
 import Header from "./header/Header";
 import Hero from "./hero/Hero";
 import "./index.css";
+
 // https://leetcode-api-faisalshohag.vercel.app/zakariaelkhalfaoui
+
 const App = () => {
+
   return (
   <div>
     <Header />
